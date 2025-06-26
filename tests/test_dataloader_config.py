91f031b8ed6665d929fe44e2247759b2676f310d
xny0115 <xnamyunx@gmail.com>
2025-06-26 13:33:47 +0900

    perf: enable cudnn benchmark and optimize dataloader

--- tests/test_dataloader_config.py
+++ tests/test_dataloader_config.py
@@ -13,7 +13,7 @@ def test_dataloader_uses_cfg(monkeypatch):
     captured = {}
     orig_loader = simple.DataLoader
 
-    def spy_loader(dataset, batch_size, shuffle, collate_fn, num_workers, pin_memory):
+    def spy_loader(dataset, batch_size, shuffle, collate_fn, num_workers, pin_memory, **kwargs):
         captured.update({'batch_size': batch_size, 'num_workers': num_workers, 'pin_memory': pin_memory})
         return orig_loader(dataset, batch_size=1, shuffle=shuffle, collate_fn=collate_fn, num_workers=0, pin_memory=False)
 
